7571ce8bab81e7709e0a09f8eb860b81c5369fae
Zack Sheppard <z@zack.io>
2014-10-22 09:18:44 -0400

    add promt to chat log

--- js/chat.js
+++ js/chat.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
   chat();
+  fadeOut();
 });
 
 function chat(){
   $("form").submit(function(){
     var chatInput = $(this).find("input:first").val();
     var id = $(this).attr('id')
-    $('ul').append('<li id="' + id + '" > ' + chatInput + '</li>');
+    $('ul').append('<li id="' + id + '" > > ' + chatInput + '</li>');
     event.preventDefault();
     this.reset();
   })
+}
+
+function fadeOut(){
+  $("li").after(function(){
+    $(this).hide(4000);
+  })
+  
 }
