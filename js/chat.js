2a66e59745773726ae55eb2302bb6817d437e20f
Zack Sheppard <z@zack.io>
2014-10-21 21:38:27 -0400

    form is working

--- js/chat.js
+++ js/chat.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+  chat();
 
+});
 
+function chat(){
+  $(".form").submit(function(){
+    $('ul').append('<li>' + $('#user-input').val() + '</li>');
+    event.preventDefault();
+  })
 }
