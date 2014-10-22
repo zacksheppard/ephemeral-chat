5a4925afb4aa790125a74bdacc4313136a6e1539
Zack Sheppard <z@zack.io>
2014-10-22 11:18:34 -0400

    add name set feature

--- js/chat.js
+++ js/chat.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
   chat();
-  fadeOut();
 });
 
 function chat(){
   $("form").submit(function(){
     var chatInput = $(this).find("input:first").val();
-    var id = $(this).attr('id')
-    $('ul').append('<li id="' + id + '" > > ' + chatInput + '</li>');
-    event.preventDefault();
-    this.reset();
+    if (chatInput.match("/name")) {
+      name = chatInput.replace(/\/name\s*/, '');
+      $(this).data('name', name);
+      $(this).find("input:first").attr('placeholder', $(this).data('name') + ' says...' ); 
+      event.preventDefault();
+      this.reset();
+    } else { 
+      var id = $(this).attr('id')
+      $('ul').append('<li id="' + id + '" > > ' + chatInput + '</li>');
+      event.preventDefault();
+      this.reset();
+    }
   })
 }
 
-function fadeOut(){
-  $("li").after(function(){
-    $(this).hide(4000);
-  })
-  
-}
